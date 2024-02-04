e0aab13b96f033e36df7174056f8ffe5809cd8c8
alinaghale88 <alinaghale.official@gmail.com>
2024-02-04 10:09:59 -0800

    Move showNav function to design-system.js

---
+++ design-system.js
@@ -0,0 +1,10 @@
+function showNav() {
+    var element1 = document.getElementsByClassName("top-navbar");
+    var element2 = document.getElementsByClassName("side-navbar");
+    for (var i = 0; i < element1.length; i++) {
+      element1[i].classList.toggle("show-items");
+  }
+  for (var i = 0; i < element2.length; i++) {
+      element2[i].classList.toggle("show-items");
+  }
+  }
