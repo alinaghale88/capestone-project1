84443a55f0d8378c9a9b1aa79c7045ac34bf8957
alinaghale88 <alinaghale.official@gmail.com>
2024-02-04 21:52:05 -0800

    Add html and JS script for displaying close button

--- design-system.js
+++ design-system.js
@@ -1,10 +1,14 @@
 function showNav() {
-    var element1 = document.getElementsByClassName("top-navbar");
-    var element2 = document.getElementsByClassName("side-navbar");
-    for (var i = 0; i < element1.length; i++) {
-      element1[i].classList.toggle("show-items");
+  var element1 = document.getElementsByClassName("top-navbar");
+  var element2 = document.getElementsByClassName("side-navbar");
+  var toggleButton = document.getElementById("toggleButton");
+  var closeButton = document.getElementById("closeButton");
+  for (var i = 0; i < element1.length; i++) {
+    element1[i].classList.toggle("navbar--show-items");
   }
   for (var i = 0; i < element2.length; i++) {
-      element2[i].classList.toggle("navbar--show-items");
+    element2[i].classList.toggle("navbar--show-items");
   }
-  }
+  toggleButton.style.display = toggleButton.style.display === 'none' ? 'block' : 'none';
+    closeButton.style.display = closeButton.style.display === 'none' ? 'block' : 'none';
+}
