70923953377ce32aea500447cd0da04908f0abd3
alinaghale88 <alinaghale.official@gmail.com>
2024-02-10 09:25:55 -0800

    Replace JS code to set active state for nav links

--- design-system.js
+++ design-system.js
@@ -16,7 +16,14 @@ function showNav() {
 }
 
 // Code to set the active state
-var currentPage = window.location.pathname;
-if (currentPage === "/about-me.html") {
-  document.getElementById("aboutLink").classList.add("link--active");
-}
+const navLinks = document.querySelectorAll('.c-top-navbar__link, .c-side-navbar__link'); // Get all navigation links
+
+const currentPageUrl = window.location.href; // Get the current page URL
+
+navLinks.forEach(link => {
+    // Check if the link href matches the current page URL
+    if (link.href === currentPageUrl) {
+        // Add a class to mark it as active
+        link.classList.add('link--active');
+    }
+});
