36f8ebbf2e2d2d180c23a93708cc14862484fb10
alinaghale88 <alinaghale.official@gmail.com>
2024-02-11 03:43:06 -0800

    Format design-system.js document

--- design-system.js
+++ design-system.js
@@ -16,14 +16,16 @@ function showNav() {
 }
 
 // Code to set the active state
-const navLinks = document.querySelectorAll('.c-top-navbar__link, .c-side-navbar__link'); // Get all navigation links
+const navLinks = document.querySelectorAll(
+  ".c-top-navbar__link, .c-side-navbar__link"
+); // Get all navigation links
 
 const currentPageUrl = window.location.href; // Get the current page URL
 
-navLinks.forEach(link => {
-    // Check if the link href matches the current page URL
-    if (link.href === currentPageUrl) {
-        // Add a class to mark it as active
-        link.classList.add('link--active');
-    }
+navLinks.forEach((link) => {
+  // Check if the link href matches the current page URL
+  if (link.href === currentPageUrl) {
+    // Add a class to mark it as active
+    link.classList.add("link--active");
+  }
 });
