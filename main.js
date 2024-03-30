5a92aa4d17260a70e2047cdd7063e487b10d4a82
alinaghale88 <alinaghale.official@gmail.com>
2024-03-29 17:35:51 -0700

    Change class name for navbar & menu block

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Code to make hamburger menu functional
 function showNav() {
-    var element1 = document.getElementsByClassName("c-nav-menu");
+    var element1 = document.getElementsByClassName("c-menu-block");
     var toggleButton = document.getElementById("toggleButton");
     var closeButton = document.getElementById("closeButton");
     for (var i = 0; i < element1.length; i++) {
-      element1[i].classList.toggle("c-nav-menu--show-items");
+      element1[i].classList.toggle("c-menu-block--show-items");
     }
     toggleButton.style.display =
       toggleButton.style.display === "none" ? "block" : "none";
